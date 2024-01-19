7ac6b0dbff5057ba5cd3fab23a2f65cec380092a
arafat-sabbir <arafatshabbir8@gmail.com>
2024-01-20 00:14:35 +0600

    Added Delete Booking Routes

--- index.js
+++ index.js
@@ -96,7 +96,14 @@ async function run() {
         res.send(result);
       })
 
-      
+      // Delete A Booking Room 
+      app.delete('/api/deleteBooking/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await bookingCollections.deleteOne(query);
+        res.send(result);
+      })
+
 
     await client.db("admin").command({ ping: 1 });
     console.log(
